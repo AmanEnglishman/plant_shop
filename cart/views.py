1b891b7cfd2dd998cd1e88c9ce50cb702fd1e68b
Aman <aman13032006@gmail.com>
2025-03-31 18:13:10 +0600

    commit

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Cart, CartItem
-from .serializers import CartSerializers, CartItemSerializers
-from product.models import Plant
 from django.shortcuts import get_object_or_404
-
+from rest_framework.decorators import action
+from .models import Cart, CartItem
+from product.models import Plant
+from .serializers import CartSerializers, CartItemSerializers
 
 class CartViewSet(viewsets.ModelViewSet):
     serializer_class = CartSerializers
@@ -19,18 +19,28 @@ class CartViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(cart)
         return Response(serializer.data)
 
+    @action(detail=False, methods=['delete'], url_path='clear')
+    def clear_cart(self, request):
+        """Очищает корзину пользователя"""
+        cart = get_object_or_404(Cart, user=request.user)
+        cart.items.all().delete()
+        return Response({'message': 'Корзина очищена'}, status=status.HTTP_204_NO_CONTENT)
+
 
 class CartItemViewSet(viewsets.ModelViewSet):
     serializer_class = CartItemSerializers
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        cart = get_object_or_404(Cart, user=self.request.user)
-        return CartItem.objects.filter(cart=cart)
+        """Кешируем корзину, чтобы избежать лишнего запроса"""
+        if not hasattr(self, 'cart'):
+            self.cart = get_object_or_404(Cart, user=self.request.user)
+        return CartItem.objects.filter(cart=self.cart)
 
-    def create(self, request, *args, **kwargs):
-        cart, created = Cart.objects.get_or_create(user=request.user)
-        plant = get_object_or_404(Plant, id=request.data.get('plant_id'))
+    def perform_create(self, serializer):
+        """Создаёт или обновляет товар в корзине"""
+        plant = get_object_or_404(Plant, id=self.request.data.get('plant_id'))
+        cart, _ = Cart.objects.get_or_create(user=self.request.user)
 
         cart_item, created = CartItem.objects.get_or_create(
             cart=cart,
@@ -42,19 +52,28 @@ class CartItemViewSet(viewsets.ModelViewSet):
             cart_item.quantity += 1
             cart_item.save()
 
+        return cart_item
+
+    def create(self, request, *args, **kwargs):
+        cart_item = self.perform_create(self.get_serializer())
         serializer = self.get_serializer(cart_item)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
+        """Обновление количества товара в корзине"""
         cart_item = self.get_object()
         quantity = request.data.get('quantity')
 
-        if quantity is not None and int(quantity) > 0:
+        if quantity is None:
+            return Response({'error': 'Quantity is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        quantity = int(quantity)
+
+        if quantity > 0:
             cart_item.quantity = quantity
             cart_item.save()
-        elif int(quantity) <= 0:
+            serializer = self.get_serializer(cart_item)
+            return Response(serializer.data)
+        else:
             cart_item.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-
-        serializer = self.get_serializer(cart_item)
-        return Response(serializer.data)
